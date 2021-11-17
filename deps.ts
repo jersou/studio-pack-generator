250acb741014b5ee2d6507af5b382cc4ab0339d9
Jerem <jersou@gmail.com>
2021-11-17 13:39:16 +0100

    build: update xml@v1.0.3 → xml@2.0.1

--- deps.ts
+++ deps.ts
@@ -15,7 +15,7 @@ export { basename, join } from "https://deno.land/std@0.114.0/path/mod.ts";
 
 export { default as i18next } from "https://deno.land/x/i18next@v21.2.4/index.js";
 
-export { parse } from "https://deno.land/x/xml@v1.0.3/mod.ts";
+export { parse } from "https://deno.land/x/xml@2.0.1/mod.ts";
 
 export { default as yargs } from "https://deno.land/x/yargs@v17.2.1-deno/deno.ts";
 export type { Arguments } from "https://deno.land/x/yargs@v17.2.1-deno/deno-types.ts";
