d8589527547b5e497431a6a5b5dca34012c65e9f
Jerem <jersou@gmail.com>
2024-09-07 10:56:54 +0200

    feat(GUI): Tauri: WIP

--- gui/src-tauri/src/main.rs
+++ gui/src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::api::process::Command;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,6 +10,12 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+
+  let (mut rx, _child) = Command::new_sidecar("studio-pack-generator")
+    .expect("failed to create `studio-pack-generator` binary command").args(["--server", "--port=5555",""])
+    .spawn()
+    .expect("Failed to spawn sidecar");
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
